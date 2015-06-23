01eb9d2029fcc83f895af42714d22ef64a7c5207
rajeshwerkushwaha <rajeshwerkushwaha@gmail.com>
2015-06-23 18:59:30 +0000

    css fixed

--- tradelance/js/home.js
+++ tradelance/js/home.js
@@ -15,4 +15,40 @@ $(document).ready(function(){
     $(this).parent().addClass('active');
   });
   
+  
+  // Cache selectors
+  var lastId,
+      topMenu = $("#fullslide-nav"),
+      topMenuHeight = topMenu.outerHeight()+50,
+      // All list items
+      menuItems = topMenu.find("a"),
+      // Anchors corresponding to menu items
+      scrollItems = menuItems.map(function(){
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+      });
+
+  // Bind to scroll
+  $(window).scroll(function(){
+     // Get container scroll position
+     var fromTop = $(this).scrollTop()+topMenuHeight;
+
+     // Get id of current scroll item
+     var cur = scrollItems.map(function(){
+       if ($(this).offset().top < fromTop)
+         return this;
+     });
+     // Get the id of the current element
+     cur = cur[cur.length-1];
+     var id = cur && cur.length ? cur[0].id : "";
+
+     if (lastId !== id) {
+         lastId = id;
+         // Set/remove active class
+         menuItems
+           .parent().removeClass("active")
+           .end().filter("[href=#"+id+"]").parent().addClass("active");
+     }                   
+  });
+  
 });
