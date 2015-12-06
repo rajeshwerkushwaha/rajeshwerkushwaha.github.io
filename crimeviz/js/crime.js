4706c14e6c27f5309f7c372f8672c8aab8078a4f
Rajesh Kumar <rajeshwerkushwaha@gmail.com>
2015-12-06 23:34:53 +0000

    updated code

--- crimeviz/js/crime.js
+++ crimeviz/js/crime.js
@@ -14,6 +14,7 @@ function getFilterData(val){
     limit = 0;
     switch (val) {
         case 'time1':
+            $('.filterData1').css({'display':'block'});
             var year = [2015,2014,2013,2012,2011];
             var month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
             var day = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
